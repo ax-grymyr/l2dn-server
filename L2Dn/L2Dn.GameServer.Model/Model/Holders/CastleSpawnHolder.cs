f04c3b0b7de4929e99fdff8be31b39cce1c2a3d5
Grymyr <grymyr@proton.me>
2024-04-25 12:13:55 +0300

    Decouple CastleSpawnHolder and Location

--- L2Dn/L2Dn.GameServer.Model/Model/Holders/CastleSpawnHolder.cs
+++ L2Dn/L2Dn.GameServer.Model/Model/Holders/CastleSpawnHolder.cs
@@ -6,16 +6,20 @@ namespace L2Dn.GameServer.Model.Holders;
 /**
  * @author St3eT
  */
-public class CastleSpawnHolder: Location
+public class CastleSpawnHolder
 {
+	private readonly Location _location;
 	private readonly int _npcId;
 	private readonly CastleSide _side;
 
-	public CastleSpawnHolder(int npcId, CastleSide side, int x, int y, int z, int heading): base(x, y, z, heading)
+	public CastleSpawnHolder(int npcId, CastleSide side, Location location)
 	{
+		_location = location;
 		_npcId = npcId;
 		_side = side;
 	}
+
+	public Location Location => _location;
 
 	public int getNpcId()
 	{
