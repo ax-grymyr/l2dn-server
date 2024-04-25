fdab69e9dda558ac1b7163f3d1cac9cb4c4a91a5
Grymyr <grymyr@proton.me>
2024-04-25 11:36:21 +0300

    Remove Point2D class

--- L2Dn/L2Dn.GameServer.Model/Model/Location.cs
+++ L2Dn/L2Dn.GameServer.Model/Model/Location.cs
@@ -1,37 +1,29 @@
 ﻿using L2Dn.GameServer.Model.Interfaces;
-using L2Dn.GameServer.Utilities;
 
 namespace L2Dn.GameServer.Model;
-
-// public readonly record struct Location(int X, int Y, int Z)
-// {
-//     // world dimensions: 1048576 * 1048576 = 1099511627776
-//     public static int WorldXMin = -655360;
-//     public static int WorldXMax = 393215;
-//     public static int WorldYMin = -589824;
-//     public static int WorldYMax = 458751;
-//
-//     public Location AddX(int dx) => this with { X = X + dx };
-//     public Location AddY(int dy) => this with { Y = Y + dy };
-//     public Location AddZ(int dz) => this with { Y = Y + dz };
-// }
 
 /**
  * A datatype used to retain a 3D (x/y/z/heading) point. It got the capability to be set and cleaned.
  */
-public class Location : Point2D, IPositionable
+public class Location : IPositionable
 {
+	protected volatile int _x;
+	protected volatile int _y;
 	protected volatile int _z;
 	protected volatile int _heading;
 	
-	public Location(int x, int y, int z): base(x, y)
+	public Location(int x, int y, int z)
 	{
+		_x = x;
+		_y = y;
 		_z = z;
 		_heading = 0;
 	}
 	
-	public Location(int x, int y, int z, int heading): base(x, y)
+	public Location(int x, int y, int z, int heading)
 	{
+		_x = x;
+		_y = y;
 		_z = z;
 		_heading = heading;
 	}
@@ -39,18 +31,12 @@ public class Location : Point2D, IPositionable
 	public Location(WorldObject obj): this(obj.getX(), obj.getY(), obj.getZ(), obj.getHeading())
 	{
 	}
-	
-	public Location(StatSet set): base(set.getInt("x", 0), set.getInt("y", 0))
-	{
-		_z = set.getInt("z", 0);
-		_heading = set.getInt("heading", 0);
-	}
-	
+
 	/**
 	 * Get the x coordinate.
 	 * @return the x coordinate
 	 */
-	public override int getX()
+	public int getX()
 	{
 		return _x;
 	}
@@ -59,7 +45,7 @@ public class Location : Point2D, IPositionable
 	 * Get the y coordinate.
 	 * @return the y coordinate
 	 */
-	public override int getY()
+	public int getY()
 	{
 		return _y;
 	}
@@ -126,20 +112,21 @@ public class Location : Point2D, IPositionable
 		_heading = loc.getHeading();
 	}
 	
-	public override void clean()
+	public void clean()
 	{
-		base.clean();
+		_x = 0;
+		_y = 0;
 		_z = 0;
 	}
 	
-	public override Location clone()
+	public Location clone()
 	{
 		return new Location(_x, _y, _z);
 	}
 	
 	public override int GetHashCode()
 	{
-		return HashCode.Combine(base.GetHashCode(), _z);
+		return HashCode.Combine(_x, _y, _z);
 	}
 	
 	public override bool Equals(Object? obj)
